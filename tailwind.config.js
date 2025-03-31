761e16f2be36e7fb3ad6ede2ecebc67a079c4fe4
Jeikolol <53280663+Jeikolol@users.noreply.github.com>
2025-03-31 09:36:18 -0400

    Improving landing

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	content: ['./src/**/*.{html,js,jsx,ts,tsx,astro}'],
+	theme: {
+		extend: {
+			colors: {
+				gold: {
+					500: '#D4AF37', // Gold color
+				},
+			},
+		},
+	},
+}
