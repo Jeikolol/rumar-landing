f5cefa19685e39ad292ca867fce900aeca54fd0d
Jeikolol <53280663+Jeikolol@users.noreply.github.com>
2025-03-31 11:57:30 -0400

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 import react from '@astrojs/react'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +11,6 @@ export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
-	output: 'static',
-	trailingSlash: 'never',
+	output: 'server', // Change to 'static' if you're using static deployment
+	adapter: vercel(), // Use 'vercel/static' for static sites
 })
